597fd1cedf0b941f60a6c7ce69cc4de22ace2abf
Saurav Sharma <sauravsharma001@gmail.com>
2018-11-01 23:09:34 -0500

    Implemented some functions

--- src/sxs179830/MDS.java
+++ src/sxs179830/MDS.java
@@ -9,55 +9,213 @@ package sxs179830;
 
 // If you want to create additional classes, place them in this file as subclasses of MDS
 
+import java.util.*;
+
 public class MDS {
+
+    /**
+     * Product class to store the information regarding the product.
+     */
+    public class Product {
+
+        private long id;
+        private List<Long> description;
+        private Money price;
+
+        Product(long id, Money price, List<Long> list) {
+            this.id = id;
+            this.price = price;
+            this.description = list;
+        }
+
+        /**
+         * @return Id of the product
+         */
+        public long getId() {
+            return this.id;
+        }
+
+        /**
+         * @return Price of the product
+         */
+        public Money getPrice() {
+            return this.price;
+        }
+
+        /**
+         * Update price of the product
+         * @param price latest price of the product
+         */
+        public void setPrice(Money price) {
+            this.price = price;
+        }
+
+        /**
+         * @return Description i.e. list of long of the product
+         */
+        public List<Long> getDescription() {
+            return this.description;
+        }
+
+        /**
+         * Update the description of the product
+         * @param list list of long to set for the product
+         */
+        public void setDescription(List<Long> list) {
+            this.description = list;
+        }
+    }
+
     // Add fields of MDS here
+    private Map<Long, Product> primaryIndexById;
+    private Map<Long, Set<Long>> secondaryIndexByDescription;
 
     // Constructors
     public MDS() {
+        primaryIndexById = new TreeMap<>();
+        secondaryIndexByDescription = new HashMap<>();
     }
 
-    /* Public methods of MDS. Do not change their signatures.
-       __________________________________________________________________
-       a. Insert(id,price,list): insert a new item whose description is given
-       in the list.  If an entry with the same id already exists, then its
-       description and price are replaced by the new values, unless list
-       is null or empty, in which case, just the price is updated.
-       Returns 1 if the item is new, and 0 otherwise.
-    */
-    public int insert(long id, Money price, java.util.List<Long> list) {
-        return 0;
+
+    /**
+     * Insert(id,price,list): insert a new item whose description is given
+     * in the list.  If an entry with the same id already exists, then its
+     * description and price are replaced by the new values, unless list
+     * is null or empty, in which case, just the price is updated.
+     * Returns 1 if the item is new, and 0 otherwise.
+     * @param id product id
+     * @param price product price
+     * @param list product description as long ints
+     * @return 1 if product doesn't exists otherwise returns 0
+     */
+    public int insert(long id, Money price, List<Long> list) {
+        int flag = 0;
+        Product prod = primaryIndexById.get(id);
+        if(prod != null) {
+            if(list.size() != 0) {
+                List<Long> desc = prod.getDescription();
+                removeSecondaryIndex(id, desc);
+                prod.setDescription(list);
+                addSecondaryIndex(id, list);
+            }
+            prod.setPrice(price);
+        } else {
+            prod = new Product(id, price, list);
+            primaryIndexById.put(id, prod);
+            addSecondaryIndex(id, list);
+            flag = 1;
+        }
+        return flag;
     }
 
-    // b. Find(id): return price of item with given id (or 0, if not found).
+    /**
+     * Add secondary index for a Product with given description
+     * @param id id of the Product
+     * @param list description of product as list of long
+     */
+    private void addSecondaryIndex(long id, List<Long> list) {
+        for(long d : list) {
+            Set<Long> idList = secondaryIndexByDescription.get(d);
+            if(idList == null) {
+                idList = new TreeSet<>();
+                idList.add(id);
+            } else {
+                idList.add(id);
+            }
+        }
+    }
+
+    /**
+     * Remove the secondary index for a Product with given description
+     * @param id id of the Product
+     * @param list description of product as list of long
+     */
+    private void removeSecondaryIndex(long id, List<Long> list) {
+        for(long d : list) {
+            Set<Long> idList = secondaryIndexByDescription.get(d);
+            if(idList != null && idList.size() > 0) {
+                idList.remove(id);
+            }
+        }
+    }
+
+    /**
+     * Find(id): return price of item with given id (or 0, if not found).
+     * @param id product id to be searched
+     * @return price of the product
+     */
     public Money find(long id) {
+        Product product = primaryIndexById.get(id);
+        if(product != null) return product.getPrice();
         return new Money();
     }
 
-    /*
-       c. Delete(id): delete item from storage.  Returns the sum of the
-       long ints that are in the description of the item deleted,
-       or 0, if such an id did not exist.
-    */
+
+    /**
+     * Delete(id): delete item from storage.  Returns the sum of the
+     * long ints that are in the description of the item deleted,
+     * or 0, if such an id did not exist.
+     * @param id id of the product to be deleted
+     * @return return sum of the long ints in description if id exists otherwise returns 0
+     */
     public long delete(long id) {
+        Product productToDelete = primaryIndexById.get(id);
+        if(productToDelete != null) {
+            primaryIndexById.remove(id);
+            long count = 0;
+            for(long l : productToDelete.getDescription()) {
+                count += l;
+            }
+            removeSecondaryIndex(id, productToDelete.getDescription());
+            return count;
+        }
         return 0;
     }
 
-    /*
-       d. FindMinPrice(n): given a long int, find items whose description
-       contains that number (exact match with one of the long ints in the
-       item's description), and return lowest price of those items.
-       Return 0 if there is no such item.
-    */
+    /**
+     * FindMinPrice(n): given a long int, find items whose description
+     * contains that number (exact match with one of the long ints in the
+     * item's description), and return lowest price of those items.
+     * @param n long value in description
+     * @return min price of the product with given description
+     */
     public Money findMinPrice(long n) {
+        Set<Long> idList = secondaryIndexByDescription.get(n);
+        Money minPrice = null;
+        if(idList != null && idList.size() > 0) {
+            for(long l : idList) {
+                Money newMoney = primaryIndexById.get(l).getPrice();
+                if(minPrice == null) minPrice = newMoney;
+                else if(minPrice.compareTo(newMoney) >= 0 ) {
+                    minPrice = newMoney;
+                }
+            }
+            return minPrice;
+        }
         return new Money();
     }
 
-    /*
-       e. FindMaxPrice(n): given a long int, find items whose description
-       contains that number, and return highest price of those items.
-       Return 0 if there is no such item.
-    */
+
+    /**
+     * FindMaxPrice(n): given a long int, find items whose description
+     * contains that number, and return highest price of those items.
+     * Return 0 if there is no such item.
+     * @param n long value in description
+     * @return max price of the product with given description
+     */
     public Money findMaxPrice(long n) {
+        Set<Long> idList = secondaryIndexByDescription.get(n);
+        Money maxPrice = null;
+        if(idList != null && idList.size() > 0) {
+            for(long l : idList) {
+                Money newMoney = primaryIndexById.get(l).getPrice();
+                if(maxPrice == null) maxPrice = newMoney;
+                else if(maxPrice.compareTo(newMoney) <= 0 ) {
+                    maxPrice = newMoney;
+                }
+            }
+            return maxPrice;
+        }
         return new Money();
     }
 
@@ -103,10 +261,19 @@ public class MDS {
         }
         public long dollars() { return d; }
         public int cents() { return c; }
-        public int compareTo(Money other) { // Complete this, if needed
-            return 0;
+
+        public int compareTo(Money other) {
+            int cmp = Long.compare(this.dollars(), other.dollars());
+            if(cmp == 0) {
+                return Integer.compare(this.cents(), other.cents());
+            } else {
+                return cmp;
+            }
         }
         public String toString() { return d + "." + c; }
     }
 
+    public static void main(String[] args) {
+
+    }
 }
